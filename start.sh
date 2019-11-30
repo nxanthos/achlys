a59136366aafc9a702a1451c41f7212d6f1647e5
Julien Banken <julien.banken@student.uclouvain.be>
2019-11-30 16:19:33 +0100

    Add script to set network topology

--- start.sh
+++ start.sh
@@ -1,24 +1,39 @@
 #!/bin/bash
 
-NAME=$(hostname -s)
+# Configuration :
+
 COOKIE='MyCookie'
+NAME=$(hostname -s)
+
+# Networking :
 
 IP=$(ifconfig | grep 'inet ' | grep -m 1 -Fv 127.0.0.1 | awk '{print $2}')
-PEER_IP=$(ifconfig | grep 'inet ' | grep -m 1 -Fv 127.0.0.1 | awk '{print $2}' | sed 's/\./,/g')
+PEER_IP=$(echo $IP | sed 's/\./,/g')
 PEER_PORT=27000
 
-SNAMES=()
-for k in {1..2}
+# Board :
+
+function SPAWN_BOARD() {
+  NAME=$NAME \
+  PEER_IP=$PEER_IP \
+  PEER_PORT="$PEER_PORT" \
+  IP=$IP \
+  BOARDS=$2 \
+  xterm -hold -e "rebar3 as test shell --sname ${1} --setcookie $COOKIE --apps achlys" &
+}
+
+PATTERN='^\(([^\)]+)\)\[(.+)\]$'
+
+python3 ./utils/graph.py --n=3 --hostname="${NAME}" |
+while IFS= read -r LINE
 do
-  SNAMES+=("achlys_${k}@${NAME}")
+  if [[ $LINE =~ $PATTERN ]]; then
+    SNAME="${BASH_REMATCH[1]}"
+    BOARDS="${BASH_REMATCH[2]}"
+    SPAWN_BOARD "${SNAME}" "${BOARDS}" "${PEER_PORT}"
+    ((PEER_PORT = PEER_PORT + 1))
+    # sleep 3
+  fi
 done
 
-BOARDS=$(printf "%s, " "${SNAMES[@]}")
-BOARDS=${BOARDS::-2}
-
-for k in "${!SNAMES[@]}"
-do
-  echo -e "${SNAMES[k]}"
-  # NAME=$NAME PEER_IP=$PEER_IP PEER_PORT=$((PEER_PORT + k)) IP=$IP BOARDS=$BOARDS xterm -e "nohup rebar3 as test shell --sname ${SNAMES[k]} --setcookie $COOKIE --apps achlys" &
-  NAME=$NAME PEER_IP=$PEER_IP PEER_PORT=$((PEER_PORT + k)) IP=$IP BOARDS=$BOARDS xterm -e "rebar3 as test shell --sname ${SNAMES[k]} --setcookie $COOKIE --apps achlys" &
-done
+read -p "Done !"
