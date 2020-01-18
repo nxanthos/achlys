29017e03ff2afca96abf0aa9033505aa51961e3c
Julien Banken <julien.banken@student.uclouvain.be>
2020-01-18 13:59:00 +0100

    Cleaning map-reduce

--- src/achlys_map_reduce.erl
+++ src/achlys_map_reduce.erl
@@ -1,16 +1,47 @@
 -module(achlys_map_reduce).
 
 -export([
+    get_size/0,
     debug/0,
     schedule_map/0,
     schedule_reduce/0
 ]).
 
+reduce(Pair_1, Pair_2, ID) ->
+    case {Pair_1, Pair_2} of {
+        #{value := V1},
+        #{value := V2}
+    } -> #{
+        id => ID,
+        key => test,
+        value => V1 + V2
+    } end.
+
 % Map phase :
 
-% TODO
+map_phase() ->
+    ID = {<<"pairs">>, state_twopset},
+    lists:foreach(fun(K) ->
+        lasp:update(ID, {add, #{
+            id => K,
+            key => test,
+            value => K
+        }}, self())
+    end, lists:seq(1, 150)).
 
 % Reduce phase :
+
+get_pairs() ->
+    {ok, Set} = lasp:query({<<"pairs">>, state_twopset}),
+    List = sets:to_list(Set),
+    lists:foldl(fun(Pair, Acc) ->
+        case Pair of #{id := ID, key := Key, value := Value} ->
+            maps:put(ID, #{
+                key => Key,
+                value => Value
+            }, Acc)
+        end
+    end, #{}, List).
 
 get_constraints(K) ->
     get_constraints(lists:seq(1, K), K + 1).
@@ -26,55 +57,43 @@ get_constraints(L, K) ->
         [] -> []
     end.
 
-reduce(Pair_1, Pair_2, ID) ->
-    case {Pair_1, Pair_2} of {
-        #{key := K1, value := V1},
-        #{key := K2, value := V2}
-    } when K1 == K2 ->
-        #{
-            id => ID,
-            key => K1,
-            value => V1 + V2
-        }
-    end.
-
 reduction_loop(Constraints) ->
-    timer:sleep(rand:uniform(100)), % Delay
-    {ok, Set} = lasp:query({<<"pairs">>, state_twopset}),
-    Pairs = sets:to_list(Set),
-    case erlang:length(Pairs) of
-        Length when Length > 1 ->
+    timer:sleep(rand:uniform(100)), % Simulate delay
+    Pairs = get_pairs(),
+    case maps:size(Pairs) of
+        Size when Size > 1 ->
             reduction_step(Constraints, Pairs),
             reduction_loop(Constraints);
-        Length when Length =< 1 ->
+        Size when Size =< 1 ->
             io:format("~n~p~n", [Pairs])
     end.
 
 reduction_step(Constraints, Pairs) ->
+    ID = {<<"pairs">>, state_twopset},
     case Constraints of
         [{{A, B}, C}|T] ->
             case {
-                lists:search(fun(E) ->
-                    case E of #{id := ID} -> ID == A end
-                end, Pairs),
-                lists:search(fun(E) ->
-                    case E of #{id := ID} -> ID == B end
-                end, Pairs)
-            } of {{value, Pair_1}, {value, Pair_2}} ->
-                ID = {<<"pairs">>, state_twopset},
+                maps:is_key(A, Pairs),
+                maps:is_key(B, Pairs)
+            } of {true, true} ->
+                Pair_1 = maps:put(id, A, maps:get(A, Pairs)),
+                Pair_2 = maps:put(id, B, maps:get(B, Pairs)),
                 lasp:update(ID, {add,
                     reduce(Pair_1, Pair_2, C)
                 }, self()),
                 lasp:update(ID, {rmv, Pair_1}, self()),
                 lasp:update(ID, {rmv, Pair_2}, self()),
-                io:format("Reduction done!~n");
+                io:format("Node left: ~p~n", [get_size()]);
             _ -> reduction_step(T, Pairs) end;
         [] -> ok
     end.
 
 reduce_phase(Nodes) ->
     Task = achlys:declare(mytask, all, single, fun() ->
-        io:format("Starting the task"),
+        % io:format("Waiting convergence...~n"),
+        % ID = {<<"pairs">>, state_twopset},
+        % lasp:read(ID, {cardinality, 150}),
+        io:format("Starting the task...~n"),
         Constraints = get_constraints(150),
         reduction_loop(Constraints)
     end),
@@ -85,14 +104,7 @@ reduce_phase(Nodes) ->
 schedule_map() ->
     lasp:declare({<<"n">>, state_gset}, state_gset),
     lasp:declare({<<"pairs">>, state_twopset}, state_twopset),
-    ID = {<<"pairs">>, state_twopset},
-    lists:foreach(fun(K) ->
-        lasp:update(ID, {add, #{
-            id => K,
-            key => test,
-            value => K
-        }}, self())
-    end, lists:seq(1, 150)).
+    map_phase().
 
 schedule_reduce() ->
     % Wait convergence before calling the reduce
@@ -101,8 +113,11 @@ schedule_reduce() ->
         'achlys2@192.168.1.6'
     ]).
 
+get_size() ->
+    {ok, Set} = lasp:query({<<"pairs">>, state_twopset}),
+    sets:size(Set).
+
 debug() ->
     {ok, Set} = lasp:query({<<"pairs">>, state_twopset}),
-    % Pairs = sets:to_list(Set),
-    % io:format("Pairs: ~p~n", [Pairs]),
-    io:format("Size: ~p~n", [sets:size(Set)]).
+    Pairs = sets:to_list(Set),
+    io:format("Pairs: ~p~n", [Pairs]).
