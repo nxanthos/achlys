9eea3fff2b764edd37208e907845b8a48f22171c
Julien Banken <julien.banken@student.uclouvain.be>
2020-02-01 00:47:50 +0100

    Example - Get prime number with event sourcing

---
+++ src/achlys_es.erl
@@ -0,0 +1,96 @@
+-module(achlys_es).
+-behaviour(gen_server).
+-export([
+    init/1,
+    handle_call/3,
+    handle_cast/2,
+    handle_info/2
+]).
+-export([
+    start_link/3,
+    read/0,
+    update/1,
+    debug/0
+]).
+
+-define(SERVER, ?MODULE).
+
+% @pre -
+% @post -
+start_link(ID, State, Handler) ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [
+        ID,
+        State,
+        Handler
+    ], []).
+
+% ID - ID of the variable
+% State - Initial state
+% Handler - Update function
+init([ID, State, Handler]) ->
+    lasp:declare(ID, state_gset),
+    lasp:stream(ID, fun(Events) ->
+        gen_server:cast(?SERVER, {propagate, Events})
+    end),
+    {ok, #{
+        variable => ID,
+        cache => sets:new(),
+        state => State,
+        handler => Handler
+    }}.
+
+% Synchronous:
+handle_call(read, _From, Data) ->
+    case Data of #{state := State} ->
+        {reply, State, Data}
+    end;
+handle_call(_Request, _From, Data) ->
+    {noreply, Data}.
+
+% Asynchronous:
+
+handle_cast({add_event, Event}, Data) ->
+    case Data of #{variable := ID} ->
+        lasp:update(ID, {add, Event}, self()),
+        {noreply, Data}
+    end;
+handle_cast({propagate, E2}, Data) ->
+    case Data of #{
+        cache := E1,
+        state := State,
+        handler := Handler
+    } ->
+        Events = sets:to_list(sets:subtract(E2, E1)),
+        {noreply, maps:merge(Data, #{
+            cache => E2,
+            state => lists:foldl(fun(Event, Current) ->
+                erlang:apply(Handler, [Event, Current])
+            end, State, Events)
+        })}
+    end;
+handle_cast(debug, Data) ->
+    io:format("Data=~p~n", [Data]),
+    {noreply, Data};
+handle_cast(_Request, Data) ->
+    {noreply, Data}.
+
+handle_info(_Info, Data) ->
+    {noreply, Data}.
+
+% API:
+
+% @pre -
+% @post -
+read() ->
+    gen_server:call(?SERVER, read).
+
+% @pre -
+% @post -
+update(Fun) ->
+    State = read(),
+    erlang:apply(Fun, [State, fun(Event) ->
+        gen_server:cast(?SERVER, {add_event, Event})
+    end]).
+
+debug() ->
+    gen_server:cast(?SERVER, debug).
---
+++ src/prime_numbers.erl
@@ -0,0 +1,89 @@
+-module(prime_numbers).
+-export([
+    debug/0,
+    schedule/0
+]).
+
+% Helpers :
+
+% @pre -
+% @post -
+is_prime(1) -> false;
+is_prime(K) ->
+    is_prime(K, 2, erlang:trunc(math:sqrt(K)) + 1).
+is_prime(_, Max, Max) -> true;
+is_prime(K, I, Max) ->
+    case (K rem I) =:= 0 of
+        true -> false;
+        _ -> is_prime(K, I + 1, Max)
+    end.
+
+% @pre -
+% @post -
+event_listener({is_prime, K}, State) ->
+    case State of #{
+        numbers := Numbers,
+        solutions := Solutions
+    } -> #{
+        numbers => lists:delete(K, Numbers),
+        solutions => [K|Solutions]
+    } end;
+event_listener({is_not_prime, K}, State) ->
+    case State of #{
+        numbers := Numbers,
+        solutions := Solutions
+    } -> #{
+        numbers => lists:delete(K, Numbers),
+        solutions => Solutions
+    } end.
+
+% @pre -
+% @post -
+update(State, Emit) ->
+    case State of
+        #{numbers := []} -> State;
+        #{numbers := [K|_]} ->
+            case is_prime(K) of
+                true -> Emit({is_prime, K});
+                _ -> Emit({is_not_prime, K})
+            end
+    end.
+
+% @pre -
+% @post -
+loop() ->
+    case achlys_es:read() of
+        #{numbers := []} -> ok;
+        #{numbers := [_|_]} ->
+            achlys_es:update(fun update/2),
+            timer:sleep(100),
+            loop()
+    end.
+
+% @pre -
+% @post -
+schedule() ->
+    ID = {<<"events">>, state_gset},
+    State = #{
+        numbers => lists:seq(1, 10),
+        solutions => []
+    },
+    Task = achlys:declare(mytask, all, single, fun() ->
+        io:format("Execution of the task~n"),
+        achlys_es:start_link(ID, State,
+            fun event_listener/2
+        ),
+        loop(),
+        case achlys_es:read() of #{solutions := Solutions} ->
+            io:format("Solution: ~p~n", [Solutions])
+        end
+    end),
+    achlys:bite(Task).
+
+% @pre -
+% @post -
+debug() ->
+    ID = {<<"events">>, state_gset},
+    {ok, Set} = lasp:query(ID),
+    Content = sets:to_list(Set),
+    io:format("Variable: ~p~n", [Content]).
