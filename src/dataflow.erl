47fc85430a41ee318ded72a8c153ae5d4b9bd5e5
Julien Banken <julien.banken@student.uclouvain.be>
2020-02-05 11:47:56 +0100

    Dataflow - Add map test + lasp_process example

--- src/dataflow.erl
+++ src/dataflow.erl
@@ -1,18 +1,21 @@
 -module(dataflow).
 -export([
-    test_dag_link/0
+    test_map/0,
+    test_fold/0,
+    test_dag_link/0,
+    test_process/0,
+    debug/0
 ]).
 
 % @pre -
 % @post -
 test_dag_link() ->
 
-    Type = state_gset,
-    IVar = {<<"ivar-process">>, Type},
-    OVar = {<<"ovar-process">>, Type},
+    IVar = {<<"ivar-process">>, state_orset},
+    OVar = {<<"ovar-process">>, state_orset},
 
-    lasp:declare(IVar, state_gset),
-    lasp:declare(OVar, state_gset),
+    lasp:declare(IVar, state_orset),
+    lasp:declare(OVar, state_orset),
 
     lasp_process:start_dag_link([
         [{IVar, fun(ID, Threshold) -> % Read function
@@ -30,10 +33,10 @@ test_dag_link() ->
                 Values % Identity transformation
             end
         end,
-        {OVar, fun(OVar, Values) -> % Write function
+        {OVar, fun(ID, Values) -> % Write function
             io:format("Write OVar: ~p~n", [OVar]),
             io:format("Write Value: ~p~n", [Values]),
-            lasp:update(OVar, {add, Values}, self())
+            lasp:update(ID, {add, Values}, self())
         end}
     ]),
 
@@ -48,10 +51,113 @@ test_dag_link() ->
 
 % @pre -
 % @post -
+test_map() ->
+
+    IVar = {<<"ivar-process">>, state_orset},
+    OVar = {<<"ovar-process">>, state_orset},
+
+    lasp:declare(IVar, state_orset),
+    lasp:declare(OVar, state_orset),
+
+    achlys_util:repeat(10, fun(K) ->
+        lasp:update(IVar, {add, K}, self())
+    end),
+
+    Fun = fun(K) -> K + 1 end,
+    lasp:map(IVar, Fun, OVar),
+    timer:sleep(500),
+
+    debug("ivar-process"),
+    debug("ovar-process"),
+    ok.
+
+% @pre -
+% @post -
+test_fold() ->
+
+    IVar = {<<"ivar-process">>, state_orset},
+    OVar = {<<"ovar-process">>, state_orset},
+
+    lasp:declare(IVar, state_orset),
+    lasp:declare(OVar, state_orset),
+
+    achlys_util:repeat(10, fun(K) ->
+        lasp:update(IVar, {add, K}, self())
+    end),
+
+    % TODO:
+    % Fun = fun(...) -> ... end,
+    % lasp:fold(IVar, Fun, OVar),
+    % timer:sleep(500),
+
+    debug("ivar-process"),
+    debug("ovar-process"),
+    ok.
+
+% @pre -
+% @post -
+test_process() ->
+
+    IVar = {<<"ivar-process">>, state_orset},
+    OVar = {<<"ovar-process">>, state_orset},
+
+    lasp:declare(IVar, state_orset),
+    lasp:declare(OVar, state_orset),
+
+    {ok, S1} = lasp_process:init([
+        [{IVar, fun(ID, Threshold) -> % Read function
+            io:format("Read ID: ~p~n", [ID]),
+            io:format("Read Threshold: ~p~n", [Threshold]),
+            lasp:read(ID, Threshold) % Synchronous read
+        end}],
+        fun(Value) -> % Transformation function
+            io:format("Transform Value: ~p~n", [Value]),
+            Value
+        end,
+        {OVar, fun(ID, Value) -> % Write function
+            io:format("Write ID: ~p~n", [ID]),
+            io:format("Write Value: ~p~n", [Value]),
+            lasp:update(ID, {add, Value}, self())
+        end}
+    ]),
+
+    io:format("State S1: ~w~n", [S1]),
+
+    % The first argument of `lasp_process:process` is a list containing the arguments that will be passed to the transformation function. The arity of the transformation function will have to correspond to the number of items present in the list.
+
+    {ok, R} = lasp_process:process([42], S1),
+    io:format("R: ~w~n", [R]),
+    
+    lasp:update(IVar, {add, 10}, self()),
+
+    % Synchronous read
+    case lasp_process:read(S1) of {ok, Functions, S2} ->
+        lists:foreach(fun(Function) ->
+            Info = erlang:fun_info(Function, arity),
+            Value = erlang:apply(Function, [undefined]),
+            io:format("Info: ~p~n", [Info]),
+            io:format("State S2: ~p~n", [S2]),
+            io:format("Value: ~p~n", [Value])
+        end, Functions)
+    end,
+
+    timer:sleep(500),
+    debug("ivar-process"),
+    debug("ovar-process"),
+    ok.
+
+% @pre -
+% @post -
+debug() ->
+    debug("ivar-process"),
+    debug("ovar-process").
+
+% @pre -
+% @post -
 debug(Name) ->
     {ok, Set} = lasp:query({
         erlang:list_to_binary(Name),
-        state_gset
+        state_orset
     }),
     Content = sets:to_list(Set),
     io:format("Variable (~p) -> ~w~n", [Name, Content]).
