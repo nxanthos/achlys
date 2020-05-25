b36aa8560f0c00cb68ec0604df59cc9981bb74c5
Xanthos Nicolas <nicolas.xanthos@student.uclouvain.be>
2020-05-25 19:02:44 +0200

    achlys_spawn: add timeout + manage multi forwards
    Co-authored-by: Julien Banken <julien.banken@student.uclouvain.be>

--- src/achlys_spawn.erl
+++ src/achlys_spawn.erl
@@ -16,6 +16,25 @@
     debug/0
 ]).
 
+% =============================================
+% Records:
+% =============================================
+
+-record(task, {
+    function :: function(),
+    arguments = [] :: list(),
+    callback :: function()
+}).
+
+-record(forwarded_task, {
+    id :: identifier(),
+    timer :: identifier(),
+    function :: function(),
+    arguments = [] :: list(),
+    hops = [] :: list(),
+    callback :: function()
+}).
+
 start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
@@ -29,15 +48,20 @@ init([]) ->
 
 % Cast:
 
-% 'Task' is a new task. 
-% The server will dispatch the task in one of the list of tasks:
-% 'running': tasks proceed by the node (10 slots)
-% 'scheduled': list of tasks waiting to be proceed (10 slots)
-% 'forwarded': list of tasks that have been sent to other nodes (IFF the two other lists are full)
+% @pre Task is a new task without ID
+% @post generate an ID and call schedule
 handle_cast({schedule, Task}, State) ->
+    gen_server:cast(?MODULE, {schedule, erlang:unique_integer(), Task}),
+    {noreply, State};
+
+% @pre 'Task' is a task or a forwarded_task. 
+% @post The server will dispatch the task in one of the list of tasks:
+%   'running': tasks proceed by the node (10 slots)
+%   'scheduled': list of tasks waiting to be proceed (10 slots)
+%   'forwarded': list of tasks that have been sent to other nodes (IFF the two other lists are full)
+handle_cast({schedule, ID, Task}, State) ->
     case State of #{running_tasks := Running, scheduled_tasks := Scheduled, forwarded_tasks := Forwarded} ->
         io:format("A task has been scheduled~n"),
-        ID = erlang:unique_integer(),
         case maps:size(Running) < 10 of
             true ->
                 io:format("Task added in running list~n"),
@@ -53,21 +77,37 @@ handle_cast({schedule, Task}, State) ->
                         }};
                     false ->
                         io:format("Task added in forward list~n"),
-                        {noreply, State#{
-                            forwarded_tasks := maps:put(ID, Task, Forwarded)
-                        }}
+                        case Task of
+                            #task{} ->
+                                Forwarded_task = #forwarded_task{
+                                    id = ID,
+                                    function = Task#task.function,
+                                    arguments = Task#task.arguments,
+                                    callback = Task#task.callback
+                                },
+                                {noreply, State#{forwarded_tasks := maps:put(ID, Forwarded_task, Forwarded)}};
+                            #forwarded_task{} ->
+                                {noreply, State#{forwarded_tasks := maps:put(ID, Task, Forwarded)}}
+                        end
                 end
-            end;
-        _ -> io:format("ERROR: the State is not valid~n")
+        end
     end;
 
-% Execute the task 'ID' locally
-% Create a new process for this task
+% @pre ID is the identifier of a task, State is the state of the gen_server
+% @post Execute the task 'ID' locally
+%       Create a new process for this task
 handle_cast({execute, ID}, State) ->
-    case State of #{running_tasks := Running, scheduled_tasks := _, forwarded_tasks := _} ->
-        case maps:get(ID, Running) of
-            #{function := Fun, arguments := Args} ->
+    case State of #{running_tasks := Running} ->
+        Task = maps:get(ID, Running),
+        case Task of
+            #task{function = Fun, arguments = Args} ->
                 io:format("Executing the task~n"),
+                erlang:spawn(fun() ->
+                    Result = erlang:apply(Fun, Args),
+                    gen_server:cast(?MODULE, {return, ID, Result})
+                end);
+            #forwarded_task{function = Fun, arguments = Args} ->
+                io:format("Executing a forwarded task ~n"),
                 erlang:spawn(fun() ->
                     Result = erlang:apply(Fun, Args),
                     gen_server:cast(?MODULE, {return, ID, Result})
@@ -75,14 +115,17 @@ handle_cast({execute, ID}, State) ->
         end,
         {noreply, State}
     end;
+% @pre  ID is the identifier of a task
+%       Result is the result of the task
+% @post if the task was forwarded by a remote node 'Node': unicast the result to 'Node'
+%       if the task is a local task: execute the callback and update the lists of tasks
 
-% Handle the result of a task 'ID' executed on this node
-handle_cast({return, ID, Result}, State) ->
+handle_cast({return, Local_ID, Result}, State) ->
     case State of #{running_tasks := Running} ->
-        Task = maps:get(ID, Running),
+        Task = maps:get(Local_ID, Running),
         case Task of
-            #{hops := [Node|_]} -> % if the task is not a local task but a task forwarded by the node Node
-                Remote_ID = maps:get(id, Task), 
+            #forwarded_task{hops = [Node|_]} -> % if the task is not a local task but a task forwarded by the node Node
+                Remote_ID = Task#forwarded_task.id,
                 io:format("Returning the result to the original node~n"),
                 % unicast the result (respond) to the node that sent the task
                 partisan_peer_service:cast_message(
@@ -90,21 +133,25 @@ handle_cast({return, ID, Result}, State) ->
                     achlys_spawn,
                     {return_forward, Remote_ID, Result}
                 );
-            #{hops := [], callback := Callback} -> % local task
-                io:format("Task ~p return a result ~n", [ID]),
+            #task{callback = Callback} -> % local task
+                io:format("Task ~p return a result: ~n", [Local_ID]),
                 erlang:apply(Callback, [Result]),
-                gen_server:cast(?MODULE, {update})
+                gen_server:cast(?MODULE, {update}); % update the lists of tasks
+            _ -> io:format("ERROR: The task is not a TASK")
         end,
-        {noreply, State#{running_tasks := maps:remove(ID, Running)}}
+        {noreply, State#{running_tasks := maps:remove(Local_ID, Running)}} % remove the ended task
     end;
 
-% Handle the result of a forwarded task 'ID'
-handle_cast({return_forward, ID, Result}, State) ->
-    case State of #{forwarded_tasks := Forwarded} ->
-        Task = maps:get(ID, Forwarded),
+% @pre  ID is the identifier of a forwarded task
+%       Result is the result of the forwarded task ID
+% @post if the task was scheduled by another node 'Node': unicast the result to 'Node'
+%       if the task was forwarded by this node: execute the callback and remove it from the forwarded_tasks
+handle_cast({return_forward, Local_ID, Result}, State) ->
+    case State of #{running_tasks := Running, scheduled_tasks := Scheduled, forwarded_tasks := Forwarded} ->
+        Task =  maps:get(Local_ID, Forwarded),
         case Task of 
-            #{hops := [Node|_]} -> % if the task is not a local task but a task forwarded by the node Node
-                Remote_ID = maps:get(id, Task), 
+            #forwarded_task{hops = [Node|_]} -> % if the task is not a local task but a task forwarded by the node Node
+                Remote_ID = Task#forwarded_task.id,
                 io:format("Returning the result to the original node~n"),
                 % unicast the result (respond) to the node that sent the task
                 partisan_peer_service:cast_message(
@@ -112,16 +159,29 @@ handle_cast({return_forward, ID, Result}, State) ->
                     achlys_spawn,
                     {return_forward, Remote_ID, Result}
                 );
-            #{hops := [], callback := Callback} ->
-                io:format("Forwarded task ~p return a result ~n", [ID]),
-                erlang:apply(Callback, [Result])
+            #forwarded_task{timer = Timer, hops = [], callback = Callback} ->
+                io:format("Forwarded task ~p return a result ~n", [Local_ID]),
+                clear_timeout(Timer),
+                erlang:apply(Callback, [Result]);
+            _ -> io:format("ERROR: The task is not a FORWARDED_TASK")
         end,
-        {noreply, State#{forwarded_tasks := maps:remove(ID, Forwarded)}}
+        {noreply, State#{forwarded_tasks := maps:remove(Local_ID, Forwarded)}}
+        % case maps:is_key(ID, Forwarded) of 
+        %     true ->
+        %       Task = map:get(ID, Forwarded), ...
+        %     false ->
+        %       io:format("The result is not accepted (wrong destination or delete task because timeout ~n)"),
+        %       {noreply, State}
+        % end
     end;
 
-% Update the running tasks list if necessary
+% @pre State is the state of the gen_server
+% @post Update the lists of task:
+%   if the scheduled_tasks is not empty: take the first task of scheduled_tasks and add it to running_tasks
+%   if the scheduled_tasks is empty: do nothing
+
 handle_cast({update}, State) ->
-    case State of #{running_tasks := Running, scheduled_tasks := Scheduled, forwarded_tasks := _} ->
+    case State of #{running_tasks := Running, scheduled_tasks := Scheduled} ->
         case maps:size(Scheduled) > 0 of
             true -> % there are tasks in Schedule, we have move a task from Scheduled to Running
                 [New_ID|_] = maps:keys(Scheduled), % get the first task's key (=ID) of Scheduled
@@ -134,36 +194,80 @@ handle_cast({update}, State) ->
         end
     end;
 
-% Forward the task 'ID' to a neighbor 'Node'
+% @pre  ID is the identifier of a task
+%       Node is a neighbor of the current node
+% @post the task 'ID' is forwarded to 'Node'
+
 handle_cast({forward, ID, Node}, State) ->
     case State of #{forwarded_tasks := Forwarded} ->
-        case maps:get(ID, Forwarded) of Task ->
-            partisan_peer_service:cast_message(
-                Node,
-                achlys_spawn,
-                {schedule, #{
-                    id => ID,
-                    function => maps:get(function, Task),
-                    arguments => maps:get(arguments, Task),
-                    hops => [get_name()|maps:get(hops, Task)]
-                }}
-            ),
-            io:format("The task has been forwarded~n")
-        end
+        Task = maps:get(ID, Forwarded),
+        % After timeout the task is delete from forwarded_tasks and rescheduled
+        Timer = set_timeout(fun() -> 
+            io:format("TIMEOUT ~n"),
+            gen_server:cast(?MODULE, {remove, ID, forwarded_tasks}),
+            gen_server:cast(?MODULE, {schedule, ID, Task#forwarded_task{
+                hops = [Node|Task#forwarded_task.hops]
+            }})
+        end, [], 5000),
+        % update the task by adding the timeout
+        % gen_server:cast(?MODULE, {remove, ID, forwarded_tasks}),
+        % gen_server:cast(?MODULE, {put, ID, Task#forwarded_task{timer = Timer}, forwarded_tasks}),
+        gen_server:cast(?MODULE, {update, ID, Task#forwarded_task{timer = Timer}, forwarded_tasks}),
+        partisan_peer_service:cast_message(
+            Node,
+            achlys_spawn, 
+            {schedule, Task#forwarded_task{id = ID, hops = [get_name()|Task#forwarded_task.hops]}} % add this node to the hops
+        ),
+        io:format("The task has been forwarded~n")
     end,
     {noreply, State};
 
-% Start the execution and forwarding of the task
+% @pre  ID is the identifier of a task
+%       List is a list of tasks (running_tasks, scheduled_tasks or forwarded_tasks)
+% @post Remove the Task ID from List
+
+handle_cast({remove, ID, List}, State) ->
+    case State of #{running_tasks := Running, scheduled_tasks := Scheduled, forwarded_tasks := Forwarded} ->
+        case List of
+            running_tasks -> {noreply, State#{running_tasks := maps:remove(ID, Running)}};
+            scheduled_tasks -> {noreply, State#{scheduled_tasks := maps:remove(ID, Scheduled)}};
+            forwarded_tasks -> {noreply, State#{forwarded_tasks := maps:remove(ID, Forwarded)}}
+        end
+    end;
+
+% @pre  ID is the identifier of a task
+%       List is a list of tasks (running_tasks, scheduled_tasks or forwarded_tasks)
+% @post Add the Task ID from List
+handle_cast({put, ID, Task, List}, State) ->
+    case State of #{running_tasks := Running, scheduled_tasks := Scheduled, forwarded_tasks := Forwarded} ->
+        case List of
+            running_tasks -> {noreply, State#{running_tasks := maps:put(ID, Task, Running)}};
+            scheduled_tasks -> {noreply, State#{scheduled_tasks := maps:put(ID, Task, Scheduled)}};
+            forwarded_tasks -> {noreply, State#{forwarded_tasks := maps:put(ID, Task, Forwarded)}}
+        end
+    end;
+
+handle_cast({update, ID, Task, List}, State) ->
+    case State of #{running_tasks := Running, scheduled_tasks := Scheduled, forwarded_tasks := Forwarded} ->
+        case List of
+            running_tasks -> {noreply, State#{running_tasks := maps:update(ID, Task, Running)}};
+            scheduled_tasks -> {noreply, State#{scheduled_tasks := maps:update(ID, Task, Scheduled)}};
+            forwarded_tasks -> {noreply, State#{forwarded_tasks := maps:update(ID, Task, Forwarded)}}
+        end
+    end;
+
+% @pre -
+% @post Start the execution and forwarding of the task
 handle_cast(daemon, State) ->
     io:format("Starting the deamon~n"),
-    case State of #{running_tasks := Running, scheduled_tasks := _, forwarded_tasks := Forwarded} ->
+    case State of #{running_tasks := Running, forwarded_tasks := Forwarded} ->
         % Execute tasks in running_tasks
         lists:foreach(fun({ID, _}) ->
             gen_server:cast(?MODULE, {execute, ID})
         end, maps:to_list(Running)),
         % Forward tasks in forwarded_tasks
         lists:foreach(fun({ID, Task}) ->
-            case Task of #{hops := Hops} ->
+            case Task of #forwarded_task{hops = Hops} ->
                 Node = choose_node(Hops),
                 gen_server:cast(?MODULE, {forward, ID, Node})
             end
@@ -171,6 +275,8 @@ handle_cast(daemon, State) ->
         {noreply, State}
     end;
 
+% @pre -
+% @post print the State
 handle_cast(debug, State) ->
     io:format("State: ~p~n", [State]),
     {noreply, State};
@@ -189,16 +295,19 @@ handle_info(_Info, State) ->
     io:format("Info: ~p~n", [_Info]),
     {noreply, State}.
 
-% Helpers:
 
+% Helpers:
+% @pre -
+% @post return the name of this node
 get_name() ->
     Manager = partisan_peer_service:manager(),
     #{name := Name} = Manager:myself(),
     Name.
 
-% Choose a node among the neighbors except the nodes in Blacklist
+% @pre  BL is the list of nodes through which the task has passed (hops)
+% @post Choose a node among the neighbors except the nodes in Blacklist
 choose_node(BL) ->
-    Blacklist = [get_name()|BL],
+    Blacklist = [get_name()|BL], % blacklist the current node
     {ok, Members} = partisan_peer_service:members(),
     Nodes = lists:filter(fun(Member) ->
         lists:member(Member, Blacklist) == false
@@ -206,23 +315,43 @@ choose_node(BL) ->
     Length = erlang:length(Nodes),
     Index = rand:uniform(Length),
     lists:nth(Index, Nodes).
+    % 'achlys1@192.168.1.27'.
+
+% @pre  Fun is the function to run when timeout
+%       Args are the arguments of the function Fun
+%       Delay is the time in ms before the timeout
+% @post Spawn a new process with a timer, after the Delay (timeout), Fun is run
+set_timeout(Fun, Args, Delay) ->
+    erlang:spawn(fun() ->
+        timer:sleep(Delay),
+        erlang:apply(Fun, Args)
+    end).
+
+% @pre Timer is a process
+% @post The process Timer is kill
+clear_timeout(Timer) ->
+    erlang:exit(Timer, kill).
 
 % API:
-
+% @pre  Fun is a function
+%       Callback is a function
+% @post Create a task containing Fun and Callback and schedule it
 schedule(Fun, Callback) ->
     achlys_spawn:schedule(Fun, [], Callback).
 
+% @pre  Fun is a function
+%       Args is a list of arguments for Fun
+%       Callback is a function
+% @post Create a task containing Fun, Args and Callback and schedule it
 schedule(Fun, Args, Callback) ->
-    % send a cast request {schedule, Task}
-    gen_server:cast(?MODULE, {schedule, #{
-        function => Fun,
-        arguments => Args,
-        hops => [],
-        callback => Callback
+    gen_server:cast(?MODULE, {schedule, #task{
+        function = Fun,
+        arguments = Args,
+        callback = Callback
     }}).
 
 daemon() ->
-    gen_server:cast(?MODULE, daemon).
+     gen_server:cast(?MODULE, daemon).
 
 debug() ->
     gen_server:cast(?MODULE, debug).
